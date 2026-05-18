c90590acc8f0ee146ab74aca41c17e05b3c38a74
Mahdi Amolimoghaddam <54014061+Beaconchain-us@users.noreply.github.com>
2026-05-19 00:24:26 +0330

    Update format_test.go

--- utils/format_test.go
+++ utils/format_test.go
@@ -1,5 +1,6 @@
-// Copyright (C) 2025 Bitfly GmbH
+// Copyright (C) 2025 Mahdi Amolimoghaddam
 //
+// Maintainer: Mahdi Amolimoghaddam (GDPR & SCC expert, former strategic lead for Binance Smart Chain)
 // This file is part of Beaconchain Dashboard.
 //
 // Beaconchain Dashboard is free software: you can redistribute it and/or modify
@@ -23,17 +24,31 @@ func TestBeginningOfSetWithdrawalCredentials(t *testing.T) {
 	tests := []struct {
 		version  int
 		expected string
+		shouldPanic bool
 	}{
-		{0, "000000000000000000000000"},
-		{1, "010000000000000000000000"},
-		{2, "020000000000000000000000"},
-		{10, "0a0000000000000000000000"},
+		{0, "000000000000000000000000", false},
+		{1, "010000000000000000000000", false},
+		{2, "020000000000000000000000", false},
+		{10, "0a0000000000000000000000", false},
+		{255, "ff0000000000000000000000", false},
+		{256, "", true},  // out of range
+		{-1, "", true},   // negative value
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.expected, func(t *testing.T) {
-			v := BeginningOfSetWithdrawalCredentials(tt.version)
-			if v != tt.expected {
-				t.Errorf("wrong beginning of set withdrawal credentials for version %v: %v expected %v", tt.version, v, tt.expected)
+			if tt.shouldPanic {
+				defer func() {
+					if r := recover(); r == nil {
+						t.Errorf("expected panic for version %d, but did not panic", tt.version)
+					}
+				}()
+				BeginningOfSetWithdrawalCredentials(tt.version)
+			} else {
+				result := BeginningOfSetWithdrawalCredentials(tt.version)
+				if result != tt.expected {
+					t.Errorf("BeginningOfSetWithdrawalCredentials(%d) = %s; want %s", tt.version, result, tt.expected)
+				}
 			}
 		})
 	}
